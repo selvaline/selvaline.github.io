b766e30f59bcaa34b763fb56e9f44df3d6615971
nico <celarda90@gmail.com>
2025-01-13 20:57:41 +0100

    developer site

---
+++ translations.js
@@ -0,0 +1,35 @@
+const translations = {
+  en: {
+    about: "About",
+    applications: "Applications",
+    contact: "Contact",
+    aboutTitle: "Selvaline",
+    aboutContent: "Let's transform ideas into cutting-edge applications, covering the typical technology stacks of web or smartphone applications.",
+    applicationsTitle: "Our Applications",
+    appName: "Italian Cadastral Map",
+    appDescription: "The best app for Italian cadastral map consultation. Check property borders, cadastral parcels, buildings, flats, houses, and borders of streets, rivers, and lakes.",
+    userReviews: "User Reviews",
+    contactTitle: "Contact Us",
+    contactContent: "Get in touch with us to learn more about how Selvaline can help you achieve your goals.",
+    contactButton: "Contact Us",
+    footerText: "© 2025 Selvaline. All rights reserved.",
+    changeLanguage: "Cambia in Italiano"
+  },
+  it: {
+    about: "Chi Siamo",
+    applications: "Applicazioni",
+    contact: "Contatti",
+    aboutTitle: "Selvaline",
+    appName: "Mappali Catasto",
+    aboutContent: "Trasformiamo idee in applicazioni all'avanguardia, coprendo gli stack tecnologici tipici delle applicazioni web o per smartphone.",
+    applicationsTitle: "Le Nostre Applicazioni",
+    appDescription: "La migliore app per la consultazione delle mappe catastali italiane. Controlla i confini delle proprietà, le particelle catastali, gli edifici, gli appartamenti, le case e i confini di strade, fiumi e laghi.",
+    userReviews: "Recensioni degli Utenti",
+    contactTitle: "Contattaci",
+    contactContent: "Mettiti in contatto con noi per saperne di più su come Selvaline può aiutarti a raggiungere i tuoi obiettivi.",
+    contactButton: "Contattaci",
+    footerText: "© 2025 Selvaline. Tutti i diritti riservati.",
+    changeLanguage: "Change to English"
+  }
+};
+
